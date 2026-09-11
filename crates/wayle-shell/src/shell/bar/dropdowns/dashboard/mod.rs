decb6ad7e89a5f19fe10ccf73e0aec68be29fabf
ggomes45 <guilhermego2008.gg@gmail.com>
2026-09-11 13:15:44 +0100

    refactor: remove unused sections and components from DashboardDropdown

--- crates/wayle-shell/src/shell/bar/dropdowns/dashboard/mod.rs
+++ crates/wayle-shell/src/shell/bar/dropdowns/dashboard/mod.rs
@@ -1,10 +1,5 @@
-mod battery_section;
-mod controls;
 mod factory;
-mod media_section;
 mod messages;
-mod network_section;
-mod quick_actions;
 mod system_stats;
 mod user_session;
 mod watchers;
@@ -20,12 +15,7 @@ use wayle_widgets::prelude::*;
 
 pub(super) use self::factory::Factory;
 use self::{
-    battery_section::{BatterySection, BatterySectionInit},
-    controls::{ControlsInit, ControlsSection},
-    media_section::{MediaSection, MediaSectionInit, MediaSectionInput},
     messages::{DashboardDropdownCmd, DashboardDropdownInit, DashboardDropdownMsg},
-    network_section::{NetworkSection, NetworkSectionInit, NetworkSectionInput},
-    quick_actions::{QuickActionsInit, QuickActionsSection},
     system_stats::{SystemStatsInit, SystemStatsInput, SystemStatsSection},
     user_session::{UserSessionInit, UserSessionSection},
 };
@@ -54,11 +44,6 @@ fn spawn_settings_app() {
 pub(crate) struct DashboardDropdown {
     scaled_width: i32,
 
-    quick_actions: Controller<QuickActionsSection>,
-    controls: Controller<ControlsSection>,
-    media: Controller<MediaSection>,
-    battery: Option<Controller<BatterySection>>,
-    network: Controller<NetworkSection>,
     system_stats: Controller<SystemStatsSection>,
     user_session: Controller<UserSessionSection>,
 }
@@ -114,18 +99,6 @@ impl Component for DashboardDropdown {
                     set_vexpand: true,
 
                     #[local_ref]
-                    quick_actions_widget -> gtk::Box {},
-
-                    #[local_ref]
-                    controls_widget -> gtk::Box {},
-
-                    #[local_ref]
-                    media_widget -> gtk::Box {},
-
-                    #[local_ref]
-                    info_row_widget -> gtk::Box {},
-
-                    #[local_ref]
                     system_stats_widget -> gtk::Box {},
 
                     #[local_ref]
@@ -140,49 +113,6 @@ impl Component for DashboardDropdown {
         root: Self::Root,
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let quick_actions = QuickActionsSection::builder()
-            .launch(QuickActionsInit {
-                network: init.network.clone(),
-                bluetooth: init.bluetooth.clone(),
-                notification: init.notification.clone(),
-                power_profiles: init.power_profiles.clone(),
-                idle_inhibit: init.idle_inhibit.clone(),
-            })
-            .detach();
-
-        let controls = ControlsSection::builder()
-            .launch(ControlsInit {
-                audio: init.audio.clone(),
-            })
-            .detach();
-
-        let media = MediaSection::builder()
-            .launch(MediaSectionInit {
-                media: init.media.clone(),
-            })
-            .detach();
-
-        let battery_svc = init
-            .battery
-            .as_ref()
-            .filter(|battery_svc| battery_svc.device.is_present.get())
-            .cloned();
-        let battery = battery_svc.map(|svc| {
-            BatterySection::builder()
-                .launch(BatterySectionInit {
-                    battery: Some(svc),
-                    power_profiles: init.power_profiles.clone(),
-                })
-                .detach()
-        });
-
-        let network = NetworkSection::builder()
-            .launch(NetworkSectionInit {
-                network: init.network.clone(),
-                sysinfo: init.sysinfo.clone(),
-            })
-            .detach();
-
         let system_stats = SystemStatsSection::builder()
             .launch(SystemStatsInit {
                 sysinfo: init.sysinfo.clone(),
@@ -204,11 +134,6 @@ impl Component for DashboardDropdown {
         let model = Self {
             scaled_width: scaled_dimension(BASE_WIDTH, scale),
 
-            quick_actions,
-            controls,
-            media,
-            battery,
-            network,
             system_stats,
             user_session,
         };
@@ -223,32 +148,11 @@ impl Component for DashboardDropdown {
         let is_visible = root.is_visible();
 
         model
-            .network
-            .emit(NetworkSectionInput::SetActive(is_visible));
-        model
             .system_stats
             .emit(SystemStatsInput::SetActive(is_visible));
-        model.media.emit(MediaSectionInput::SetActive(is_visible));
 
-        let quick_actions_widget = model.quick_actions.widget();
-        let controls_widget = model.controls.widget();
-        let media_widget = model.media.widget();
         let system_stats_widget = model.system_stats.widget();
         let user_session_widget = model.user_session.widget();
-
-        let info_row = gtk::Box::new(gtk::Orientation::Horizontal, 0);
-        info_row.add_css_class("dashboard-info-row");
-        info_row.set_homogeneous(true);
-        if let Some(battery_ctrl) = &model.battery {
-            info_row.append(battery_ctrl.widget());
-            battery_ctrl.widget().set_hexpand(true);
-            info_row.append(model.network.widget());
-            model.network.widget().set_hexpand(true);
-        } else {
-            info_row.append(model.network.widget());
-            model.network.widget().set_hexpand(true);
-        }
-        let info_row_widget = &info_row;
 
         let widgets = view_output!();
         ComponentParts { model, widgets }
@@ -257,9 +161,7 @@ impl Component for DashboardDropdown {
     fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>, root: &Self::Root) {
         match msg {
             DashboardDropdownMsg::VisibilityChanged(visible) => {
-                self.network.emit(NetworkSectionInput::SetActive(visible));
                 self.system_stats.emit(SystemStatsInput::SetActive(visible));
-                self.media.emit(MediaSectionInput::SetActive(visible));
             }
             DashboardDropdownMsg::OpenSettings => {
                 root.popdown();
